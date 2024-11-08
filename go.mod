1d6007ad5bf95db577779f908be9b4613924b38f
jj_ <avasoldier2@gmail.com>
2024-11-08 12:21:06 +0900

    ✨ domain/model/bookにプロパティ取得のためのメソッドを追加

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/Joju-Matsumoto/oreilly-notification
 
 go 1.23.0
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/stretchr/testify v1.9.0
+	golang.org/x/net v0.30.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- internal/domain/model/book.go
+++ internal/domain/model/book.go
@@ -1,6 +1,14 @@
 package model
 
-import "github.com/Joju-Matsumoto/oreilly-notification/pkg/oreillyapi"
+import (
+	"bytes"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/Joju-Matsumoto/oreilly-notification/pkg/oreillyapi"
+	"golang.org/x/net/html"
+)
 
 type Book struct {
 	Result oreillyapi.Result
@@ -18,4 +26,57 @@ func (b *Book) ID() string {
 
 func (b *Book) Title() string {
 	return b.Result.Title
+}
+
+func (b *Book) Description() string {
+	doc, _ := html.Parse(strings.NewReader(b.Result.Description))
+	return htmlString(doc)
+}
+
+func (b *Book) Cover() string {
+	return b.Result.CoverUrl
+}
+
+func (b *Book) AddedAt() time.Time {
+	return b.Result.DateAdded
+}
+
+func (b *Book) PublishedAt() time.Time {
+	return b.Result.Issued
+}
+
+func (b *Book) URL() string {
+	u, _ := url.JoinPath("https://learning.oreilly.com", b.Result.WebUrl)
+	return u
+}
+
+func (b *Book) Page() int {
+	return b.Result.VirtualPages
+}
+
+func (b *Book) Authors() []string {
+	return b.Result.Authors
+}
+
+func (b *Book) Publishers() []string {
+	return b.Result.Publishers
+}
+
+// htmlタグの削除
+func htmlString(node *html.Node) string {
+	var buf bytes.Buffer
+
+	var f func(node *html.Node)
+	f = func(node *html.Node) {
+		if node.Type == html.TextNode {
+			buf.WriteString(node.Data)
+		}
+		for c := node.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(node)
+
+	return buf.String()
 }
